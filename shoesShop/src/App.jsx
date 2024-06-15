e708b36f06fc15aad9376baadc13ec613174df48
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-06-15 21:13:09 +0330

    created Navbar component

--- shoesShop/src/App.jsx
+++ shoesShop/src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component, useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <div>
-    
-   </div>
-  )
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
-export default App
+export default App;
