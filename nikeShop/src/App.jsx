f7c6651d6c400fe16ee4fb379e83ad001b36ac6c
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-06-18 20:17:51 +0330

    updated

--- nikeShop/src/App.jsx
+++ nikeShop/src/App.jsx
@@ -2,12 +2,14 @@ import { Component, useState } from "react";
 import "./App.css";
 import ShoeList from "./components/ShoeList";
 import Navbar from "./components/Navbar";
+import Button from "@mui/material/Button";
 
 function App() {
   // Add more shoes as needed
 
   return (
     <div className="App">
+      <Button variant="contained">Hello world</Button>
       <Navbar />
       {/* Use ShoeItem here if needed */}
       <h1>hhh</h1>
