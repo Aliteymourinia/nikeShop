edb6608dd21a7434408fba8db7a4c46e0c87ac0a
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-06-21 20:03:23 +0330

    update

---
+++ nikeShop/src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+// src/components/HeroSection.jsx
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <section className="hero">
+      {/* Add your hero content here */}
+    </section>
+  );
+};
+
+export default HeroSection;
