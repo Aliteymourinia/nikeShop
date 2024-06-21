7da9146d3d2a78eeaa1f08ad1c0affce11b96cf7
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-06-21 20:02:17 +0330

    created Header component

---
+++ nikeShop/src/components/Header.jsx
@@ -0,0 +1,12 @@
+// src/components/Header.jsx
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      {/* Add your logo and navigation links here */}
+    </header>
+  );
+};
+
+export default Header;
