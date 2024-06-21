394af7c6ff47e995440c13892cf6c64d4fdb478e
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-06-21 20:06:23 +0330

    updated

---
+++ nikeShop/src/components/ProductDetails.jsx
@@ -0,0 +1,8 @@
+// src/components/ProductDetails.jsx
+import React from "react";
+
+const ProductDetails = ({ product }) => {
+  return <div className="product-details">{/* Display product details */}</div>;
+};
+
+export default ProductDetails;
