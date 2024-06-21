2ae342bb66aa82e9331c4fab26ba617a3d0df9c5
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-06-21 20:06:47 +0330

    updated

---
+++ nikeShop/src/components/Cart.jsx
@@ -0,0 +1,12 @@
+// src/components/Cart.jsx
+import React from 'react';
+
+const Cart = ({ items }) => {
+  return (
+    <aside className="cart">
+      {/* Display cart items and checkout button */}
+    </aside>
+  );
+};
+
+export default Cart;
