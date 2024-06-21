ea206e779fa77147805f79e071523d16a3a79b54
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-06-21 22:19:04 +0330

    installed tailwind by npm

--- nikeShop/src/App.jsx
+++ nikeShop/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header className="" />
+      <Header />
     </div>
   );
 }
--- nikeShop/src/components/Header.jsx
+++ nikeShop/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Logo from "../assets/Nike.jsx";
 const Header = () => {
   return (
-    <header>
+    <header className="fixed bottom-80 left-6">
       <Logo />
       {/* Add your logo and navigation links here */}
     </header>
