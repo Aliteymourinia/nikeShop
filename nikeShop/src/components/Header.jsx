4c4fd2cb6be918784fb2dff5ca61da7f97ef4fb2
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-06-22 12:04:23 +0330

    added style to Header component

--- nikeShop/src/components/Header.jsx
+++ nikeShop/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import Logo from "../assets/Nike.jsx";
 const Header = () => {
   return (
     <header className="fixed w-fit -top-40 p-2 z-50 bg-white shadow-md">
       {/* Add your logo and navigation links here */}
       <Logo />
-      
+
     </header>
   );
 };
