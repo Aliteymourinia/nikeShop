4cb4c43dd6872e1bd58370815dcb9c6c4ecc0083
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-06-22 12:10:48 +0330

    added nav to header

--- nikeShop/src/components/Header.jsx
+++ nikeShop/src/components/Header.jsx
@@ -3,11 +3,24 @@ import { AppBar, Toolbar, Typography } from "@mui/material";
 import Logo from "../assets/Nike.jsx";
 const Header = () => {
   return (
-    <header className="fixed w-fit -top-40 p-2 z-50 bg-white shadow-md">
-      {/* Add your logo and navigation links here */}
-      <Logo />
-
-    </header>
+    <AppBar
+      position="static"
+      className="w-fit -top-40 p-2 z-50 bg-white shadow-md"
+    >
+      <Toolbar>
+        <Typography variant="h" component="div" sx={{ flexGrow: 1 }}>
+          <Logo />
+          {/* Place your logo here */}
+        </Typography>
+        <nav className="flex gap-4 justify-around">
+          {/* Example navigation links */}
+          <a href="#men">Men</a>
+          <a href="#women">Women</a>
+          <a href="#kids">Kids</a>
+          <a href="#customize">Customize</a>
+        </nav>
+      </Toolbar>
+    </AppBar>
   );
 };
 
