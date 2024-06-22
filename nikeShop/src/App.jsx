d427bbb6a5a03a04e3ba1fe42e09b8c7ec7d3977
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-06-22 20:42:51 +0330

    added styles to button

--- nikeShop/src/App.jsx
+++ nikeShop/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
     <div className="App">
       <h1></h1>
       <Header />
+      <HeroSection />
     </div>
   );
 }
--- nikeShop/src/components/HeroSection.jsx
+++ nikeShop/src/components/HeroSection.jsx
@@ -1,10 +1,25 @@
 // src/components/HeroSection.jsx
-import React from 'react';
-
+import React from "react";
+import { Button, Select } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import {
+  fa0,
+  faBasketShopping,
+  faShoppingBag,
+} from "@fortawesome/free-solid-svg-icons";
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 const HeroSection = () => {
   return (
     <section className="hero">
-      {/* Add your hero content here */}
+      <h1></h1>
+      <h2></h2>
+      <img src="" alt="" />
+      <div className="buttonContainer flex">
+        <button className="px-6">
+          Shop Now
+          <ArrowCircleRightIcon className="ml-4" />
+        </button>
+      </div>
     </section>
   );
 };
