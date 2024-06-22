682c5b8c356c4b0a897bcbe2c29e23bd2b478125
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-06-22 21:32:17 +0330

    added some style to texts

--- nikeShop/src/components/HeroSection.jsx
+++ nikeShop/src/components/HeroSection.jsx
@@ -3,13 +3,19 @@ import { Button, Select } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import {} from "@fortawesome/free-solid-svg-icons";
 import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
+import nikeShoe from "../assets/nikeShoe.png";
 const HeroSection = () => {
   return (
-    <section className="hero">
-      <h1>Shoes as unique as the human who wears it.</h1>
-      <h2>We provide best shoes for every budget , as you want</h2>
+    <section className="hero flex flex-col  justify-between">
+      <h1 className="text-balance w-1/3">
+        Shoes as unique as the human who wears it.
+      </h1>
+      <h2 className="w-1/4">
+        We provide best shoes for every budget as you want , discover new
+        collections.
+      </h2>
       <h1>$319.19</h1>
-      <img src="" alt="" />
+      <img className="w-1/2" src={nikeShoe} alt="nikeShoe" />
       <div className="buttonContainer flex">
         <button className="px-6">
           Shop Now
