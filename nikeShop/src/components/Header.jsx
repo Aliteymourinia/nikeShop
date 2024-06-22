29211ce20f104bcafa46714a9ad32358498f115c
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-06-23 00:16:28 +0330

    updated

--- nikeShop/src/components/Header.jsx
+++ nikeShop/src/components/Header.jsx
@@ -9,12 +9,20 @@ const Header = () => {
     <div className="HeaderContainer flex p-7 mx-3 justify-between items-center w-full h-12 my-3">
       <Logo />
       <nav className="flex gap-12">
-        <a className="hover:underline underline-offset-4" href="#men">Men</a>
-        <a className="hover:underline underline-offset-4" href="#women">Women</a>
-        <a className="hover:underline underline-offset-4" href="#kids">Kids</a>
-        <a className="hover:underline underline-offset-4" href="#customize">Customize</a>
+        <a className="hover:underline underline-offset-4" href="#men">
+          Men
+        </a>
+        <a className="hover:underline underline-offset-4" href="#women">
+          Women
+        </a>
+        <a className="hover:underline underline-offset-4" href="#kids">
+          Kids
+        </a>
+        <a className="hover:underline underline-offset-4" href="#customize">
+          Customize
+        </a>
       </nav>
-      <div className="iconContainer flex gap-4 mx-6">
+      <div className="iconContainer flex gap-4 mx-6 text-lg">
         <a href="/search">
           <FontAwesomeIcon icon={faSearch} />
         </a>
