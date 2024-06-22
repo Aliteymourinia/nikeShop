8ee33cf4a84f6930da25dfaa12a666ae675662c4
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-06-22 19:20:22 +0330

    updated

--- nikeShop/src/components/Header.jsx
+++ nikeShop/src/components/Header.jsx
@@ -1,18 +1,12 @@
 import React from "react";
-import {
-  AppBar,
-  Card,
-  Toolbar,
-  Typography,
-  outlinedInputClasses,
-} from "@mui/material";
+import { AppBar, Card, Toolbar, Typography } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import Logo from "../assets/Nike.jsx";
 const Header = () => {
   return (
-    <div className="HeaderContainer flex p-2 mx-3 justify-between items-center w-full h-12">
+    <div className="HeaderContainer flex p-2 mx-3 justify-between items-center w-full h-12 my-3">
       <Logo />
       <nav className="flex gap-10">
         <a href="#men">Men</a>
