679b505ef9b63e1ccbfb725233965a6af098266a
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-06-22 18:18:14 +0330

    added flex to Header items

--- nikeShop/src/components/Header.jsx
+++ nikeShop/src/components/Header.jsx
@@ -4,24 +4,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import Logo from "../assets/Nike.jsx";
-import { light } from "@mui/material/styles/createPalette.js";
 const Header = () => {
   return (
-    <AppBar className="fixed top-0 left-0 right-0">
-      <Toolbar className="flex justify-center gap-40">
-        <Logo />
-        {/* Place your logo here */}
-        <nav>
-          {/* Example navigation links */}
-          <a href="#men">Men</a>
-          <a href="#women">Women</a>
-          <a href="#kids">Kids</a>
-          <a href="#customize">Customize</a>
-          <FontAwesomeIcon icon={faSearch} />
-          <FontAwesomeIcon icon={faCartShopping} />
-        </nav>
-      </Toolbar>
-    </AppBar>
+    <div className="HeaderContainer flex p-2 mx-3 justify-between items-center w-full h-12">
+      <Logo />
+      <nav className="flex gap-10">
+        <a href="#men">Men</a>
+        <a href="#women">Women</a>
+        <a href="#kids">Kids</a>
+        <a href="#customize">Customize</a>
+      </nav>
+      <div className="iconContainer flex gap-4 mx-6">
+        <FontAwesomeIcon icon={faSearch} />
+        <FontAwesomeIcon icon={faCartShopping} />
+      </div>
+    </div>
   );
 };
 
