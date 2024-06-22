8ad8fdd57603422a82eb644485d3e6f9ce2e0d38
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-06-22 23:41:09 +0330

    updated

--- nikeShop/src/components/HeroSection.jsx
+++ nikeShop/src/components/HeroSection.jsx
@@ -6,22 +6,22 @@ import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 import nikeShoe from "../assets/nikeShoe.png";
 const HeroSection = () => {
   return (
-    <section className="hero flex flex-row p-8">
-      <div>
-        <h1 className="text-balance w-2/3">
+    <section className="hero flex flex-row p-10 items-center">
+      <div className="relative left-16">
+        <h1 className="w-2/5 leading-tight ">
           Shoes as unique as the human who wears it.
         </h1>
-        <h2 className="w-2/4 mx-1">
+        <h2 className="w-80 mx-1 text-sm my-2">
           We provide best shoes for every budget as you want , discover new
           collections.
         </h2>
         <h1 className="mt-7 mx-10 font-sans">$319.19</h1>
       </div>
-      <div>
-        <img className="w-1/2" src={nikeShoe} alt="nikeShoe" />
+      <div className="flex flex-col items-center absolute w-2/5 left-[40%] top-20 ">
+        <img className="" src={nikeShoe} alt="nikeShoe" />
         <div className="buttonContainer flex">
-          <button className="px-6">
-            Shop Now
+          <button className="px-4 absolute top-[65%] left-[63%]">
+            Shop now
             <ArrowCircleRightIcon className="ml-4" />
           </button>
         </div>
