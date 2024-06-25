6403aacaa7879965a330455eb05ccbedbf2652dc
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-06-25 21:34:03 +0330

    added pages

--- nikeShop/src/App.jsx
+++ nikeShop/src/App.jsx
@@ -1,4 +1,5 @@
 import { Component, useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
 import Button from "@mui/material/Button";
 import Header from "./components/Header";
@@ -6,6 +7,9 @@ import HeroSection from "./components/HeroSection";
 import ProductDetails from "./components/ProductDetails";
 import Cart from "./components/Cart";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import ProductListing from "./components/ProductListing";
 import productsData, { products } from "./productsData";
 function App() {
