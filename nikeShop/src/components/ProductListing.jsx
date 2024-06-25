70c321a9c2aa3a601e2c151abf137283cf6cada0
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-06-25 22:57:33 +0330

    added react routers and Link

--- nikeShop/src/components/ProductListing.jsx
+++ nikeShop/src/components/ProductListing.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const ProductListing = ({ products }) => {
   const productsArray = Object.values(products);
   return (
-    <div className="flex flex-wrap">
+    <div className="flex flex-wrap w-full">
       {productsArray.map((product) => {
         return (
           <div className="bg-gray-400 m-2 w-1/4 flex flex-col" key={product.id}>
