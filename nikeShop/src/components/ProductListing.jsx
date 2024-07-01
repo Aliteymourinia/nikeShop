896c1f1d8191f3bdc432341d6eee0cce394328a0
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-01 19:46:15 +0330

    added new products

--- nikeShop/src/components/ProductListing.jsx
+++ nikeShop/src/components/ProductListing.jsx
@@ -11,12 +11,11 @@ const ProductListing = ({ products }) => {
             key={product.id}
           >
             <img className="w-full" src={product.imageURL} alt={product.name} />
-            <div className="discription flex flex-col  w-full">
+            <div className="discription flex flex-col  w-full bg-white p-2">
               <h3 className="font-bold">{product.name}</h3>
               <h2 className="text-gray-500 font-sans">{product.category}</h2>
               <h2 className="text-gray-500 font-sans">{product.gender}</h2>
-
-              <h2 className="font-bold text-lg">${product.price}</h2>
+              <h2 className="font-bold text-lg font-sans">${product.price}</h2>
             </div>
           </div>
         );
