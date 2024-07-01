df2eb64b008fe69dcf4ab8c3bc6fccdd8c1807b1
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-01 12:52:52 +0330

    created main component

--- nikeShop/src/pages/Home.jsx
+++ nikeShop/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
+import Main from "../components/Main";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <HeroSection />
+      <Main />
     </div>
   );
 };
