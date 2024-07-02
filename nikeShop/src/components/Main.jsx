be5215bb19cb4a227c8ad73af0c281e9f63d61b8
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-02 10:47:59 +0330

    added new images

--- nikeShop/src/components/Main.jsx
+++ nikeShop/src/components/Main.jsx
@@ -1,26 +1,28 @@
 import React from "react";
-import Man1 from "../assets/Man1.jpg";
-import Man2 from "../assets/Man2.jpg";
-import WhiteShoe from "../assets/WhiteShoe.jpg";
-import WhiteShoe2 from "../assets/WhiteShoe2.jpg";
+import Man1 from "../assets/Man1.png";
+import Man2 from "../assets/Man2.png";
+import WhiteShoe1 from "../assets/WhiteShoe1.png";
+import WhiteShoe2 from "../assets/WhiteShoe2.png";
 // import NikeGif from "../assets/NikeGif.jpg";
 function Main() {
   return (
     <div>
-      <section>
+      <container className="flex flex-row w-3/4 p-10 gap-5 items-center">
         <a href="">
           <img src={Man1} alt="Man" />
         </a>
         <a href="">
           <img src={Man2} alt="Man2" />
         </a>
+      </container>
+      <container className="flex flex-row p-10items-center">
         <a href="">
-          <img src={WhiteShoe} alt="WhiteShoe" />
+          <img src={WhiteShoe1} alt="WhiteShoe" />
         </a>
         <a href="">
           <img src={WhiteShoe2} alt="WhiteShoe2" />
         </a>
-      </section>
+      </container>
     </div>
   );
 }
