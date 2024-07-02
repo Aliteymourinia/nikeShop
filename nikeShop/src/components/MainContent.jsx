b57fd26c528935f02a2c57b29425861a02a20d22
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-02 13:26:31 +0330

    added new images

--- nikeShop/src/components/MainContent.jsx
+++ nikeShop/src/components/MainContent.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import Man1 from "../assets/Man1.png";
-import Man2 from "../assets/Man2.png";
+import bas from "../assets/bas.jpg";
 import WhiteShoe1 from "../assets/WhiteShoe1.png";
-import gg from "../assets/gg.jpg";
+import greenShoe from "../assets/greenShoe.png";
+import { green } from "@mui/material/colors";
 // import NikeGif from "../assets/NikeGif.jpg";
 function Main() {
   return (
     <div className="my-40 flex flex-col">
       <section className="flex ">
         <a href="">
-          
+          <div>
+            <img src="" alt="" />
+          </div>
         </a>
         <a href="">
-        
+          <div>
+            <img src="" alt="" />
+          </div>
         </a>
       </section>
       <section className="flex">
@@ -28,8 +32,8 @@ function Main() {
         <a href="">
           <div className="image-container">
             <img
-              className="w-full h-full object-cover object-bottom"
-              src={gg}
+              className="w-full h-full object-cover"
+              src={greenShoe}
               alt="GreenShoee"
             />
           </div>
