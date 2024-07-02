12a2cb151251fbfce3ffa4d6760d9f1110da8c25
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-02 10:24:33 +0330

    updated

--- nikeShop/src/components/Main.jsx
+++ nikeShop/src/components/Main.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-
+import Man1 from "../assets/Man1.jpg";
+import Man2 from "../assets/Man2.jpg";
+import WhiteShoe from "../assets/WhiteShoe.jpg";
+import WhiteShoe2 from "../assets/WhiteShoe2.jpg";
+// import NikeGif from "../assets/NikeGif.jpg";
 function Main() {
   return (
     <div>
       <section>
         <a href="">
-          <img src alt="" />
+          <img src={Man1} alt="Man" />
+        </a>
+        <a href="">
+          <img src={Man2} alt="Man2" />
+        </a>
+        <a href="">
+          <img src={WhiteShoe} alt="WhiteShoe" />
+        </a>
+        <a href="">
+          <img src={WhiteShoe2} alt="WhiteShoe2" />
         </a>
       </section>
     </div>
