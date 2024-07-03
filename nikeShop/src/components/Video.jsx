e5184b58d63d35871a8bd38438efcd8edf141dbe
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-03 20:00:50 +0330

    created video component

--- nikeShop/src/components/Video.jsx
+++ nikeShop/src/components/Video.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
 const VideoComponent = () => {
-  return(
-      <div>Video</div>
-
-      
-  )
+  return (
+    <div className=" video-container my-40 flex justify-center">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/uXlWYZ022zU?si=HC2stLr79FmRjFRW"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
 };
-
-export default Video;
+export default VideoComponent;
