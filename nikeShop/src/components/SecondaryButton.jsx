a07c6529e887b550738168c7686d501282d13e52
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-03 22:55:05 +0330

    added new text to MainComponent

--- nikeShop/src/components/SecondaryButton.jsx
+++ nikeShop/src/components/SecondaryButton.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-function SecondaryButton() {
+function SecondaryButton({ text }) {
   return (
     <div>
-      <button className="bg-white text-black px-5 font-bold hover:bg-black hover:text-white active:scale-90 ">Shop</button>
+      <button className="bg-white text-black px-5 font-bold hover:bg-black hover:text-white active:scale-90 ">
+        {text}
+      </button>
     </div>
   );
 }
