965dffb3d8d0f738d8e184c60b27cdcb8470d3e6
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-03 17:42:50 +0330

    added scale to the button

--- nikeShop/src/components/SecondaryButton.jsx
+++ nikeShop/src/components/SecondaryButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function SecondaryButton() {
   return (
     <div>
-      <button className="bg-white text-black px-5 font-bold hover:bg-black hover:text-white ">Shop</button>
+      <button className="bg-white text-black px-5 font-bold hover:bg-black hover:text-white active:scale-90 ">Shop</button>
     </div>
   );
 }
