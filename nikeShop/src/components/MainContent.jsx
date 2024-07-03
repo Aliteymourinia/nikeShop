7aaaab0ae65a53425f9c0c65aa105264e040fc33
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-03 18:06:08 +0330

    added text

--- nikeShop/src/components/MainContent.jsx
+++ nikeShop/src/components/MainContent.jsx
@@ -77,8 +77,9 @@ function Main() {
         </a>
       </section>
       <div className="">
-        <img src={NikeGif} alt="" />
+        <img className="" src={NikeGif} alt="" />
       </div>
+        <h3 className="text-3xl flex justify-center mt-20 ">Discover The New Products By Categories</h3>
     </div>
   );
 }
