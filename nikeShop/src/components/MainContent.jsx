7af09451d7b053b4124b8d386797c8298f342d71
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-07 18:02:59 +0330

    created new component Cards

--- nikeShop/src/components/MainContent.jsx
+++ nikeShop/src/components/MainContent.jsx
@@ -1,16 +1,9 @@
 import React from "react";
-import BlackShoe from "../assets/BlackShoe.jpg";
-import AirColor from "../assets/AirColor.png";
-import AirGreen from "../assets/AirGreen.png";
-import GreenShoe from "../assets/GreenShoe.jpg";
-import SecondaryButton from "../components/SecondaryButton";
-import VideoComponent from "../components/Video";
-import Hoodie from "../assets/Hoodie.jpg";
-
+import Cards from "./Cards";
 function MainContent() {
   return (
     <div className="my-40 flex flex-col">
-  
+      <Cards />
     </div>
   );
 }
