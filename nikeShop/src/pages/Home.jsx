3ac804d2b73ebfddc32ffed3a9769d009c5ebd93
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-08 20:50:20 +0330

    updated

--- nikeShop/src/pages/Home.jsx
+++ nikeShop/src/pages/Home.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
 import MainContent from "../components/MainContent";
-import Footer from "../components/Footer";
 const Home = () => {
   return (
     <div>
