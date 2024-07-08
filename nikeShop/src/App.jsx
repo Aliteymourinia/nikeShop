2bb6f8908fe382aa59bc49f876c622e17d74787f
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-08 11:32:04 +0330

    created Layout component

--- nikeShop/src/App.jsx
+++ nikeShop/src/App.jsx
@@ -13,43 +13,33 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import ProductListing from "./components/ProductListing";
 import products from "./productsData";
-import Footer from "./components/Footer";
+import Layout from "./components/Layout";
 function App() {
-  const location = useLocation();
-
-  const isProductPage =
-    location.pathname === "/men" ||
-    location.pathname === "/women" ||
-    location.pathname === "/kids" ||
-    location.pathname === "/customize";
-
   return (
-    <div className="App">
-      <Header />
-      {!isProductPage}
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/products" element={<ProductListing />}></Route>
-        <Route path="/about" element={<About />}></Route>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="products" element={<ProductListing />} />
+        <Route path="about" element={<About />} />
         <Route
-          path="/men"
+          path="men"
           element={<ProductListing products={products.men} />}
-        ></Route>
-
+        />
         <Route
-          path="/women"
+          path="women"
           element={<ProductListing products={products.women} />}
         />
         <Route
-          path="/kids"
+          path="kids"
           element={<ProductListing products={products.kids} />}
         />
         <Route
-          path="/customize"
+          path="customize"
           element={<ProductListing products={products.customize} />}
         />
-      </Routes>
-    </div>
+      </Route>
+    </Routes>
   );
 }
+
 export default App;
