0c63e7f84ebfd6ea2e666c1351d007c72788d728
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-08 11:48:08 +0330

    updated

--- nikeShop/src/components/Layout.jsx
+++ nikeShop/src/components/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <div>
+      <div className="flex-grow">
         <Outlet />
       </div>
       <Footer />
