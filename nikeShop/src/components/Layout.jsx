6db9a6058f60f64c185f6a328d0e1188e9a7da9b
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-08 11:17:15 +0330

    created Layout component

--- nikeShop/src/components/Layout.jsx
+++ nikeShop/src/components/Layout.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import Header from "./Header";
+import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex-grow">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
