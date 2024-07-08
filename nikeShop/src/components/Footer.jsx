3004f0e6ac3224fac7f8710b5c587cb13dacbfe4
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-08 13:24:59 +0330

    updated

--- nikeShop/src/components/Footer.jsx
+++ nikeShop/src/components/Footer.jsx
@@ -5,107 +5,116 @@ const Footer = () => {
   return (
     <footer className="footer-section">
       <div className="container">
-        <div className="footer-cta pt-5 pb-5">
-          <div className="row">
-            <div className="col-xl-4 col-md-4 mb-30">
-              <div className="single-cta">
-                <i className="fas fa-map-marker-alt"></i>
-                <div className="cta-text">
-                  <h4>Find us</h4>
-                  <span>1010 Avenue, sw 54321, chandigarh</span>
-                </div>
+        <div className="footer-cta pt-5 pb-5"></div>
+        <div className="footer-content ">
+          <div className="col-xl-4 col-lg-4 mb-50">
+            <div className="footer-widget">
+              <div className="footer-text"></div>
+              <div className="footer-social-icon">
+                <a href="#">
+                  <i className="fab fa-facebook-f facebook-bg"></i>
+                </a>
+                <a href="#">
+                  <i className="fab fa-twitter twitter-bg"></i>
+                </a>
+                <a href="#">
+                  <i className="fab fa-google-plus-g google-bg"></i>
+                </a>
               </div>
             </div>
-            <div className="col-xl-4 col-md-4 mb-30">
-              <div className="single-cta">
-                <i className="fas fa-phone"></i>
-                <div className="cta-text">
-                  <h4>Call us</h4>
-                  <span>9876543210 0</span>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-4 mb-30">
-              <div className="single-cta">
-                <i className="far fa-envelope-open"></i>
-                <div className="cta-text">
-                  <h4>Mail us</h4>
-                  <span>mail@info.com</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="footer-content pt-5 pb-5">
-          <div className="row">
-            <div className="col-xl-4 col-lg-4 mb-50">
-              <div className="footer-widget">
-                <div className="footer-logo"></div>
-                <div className="footer-text">
-                  <p>
-                    Lorem ipsum dolor sit amet, consec tetur adipisicing elit,
-                    sed do eiusmod tempor incididuntut consec tetur adipisicing
-                    elit,Lorem ipsum dolor sit amet.
-                  </p>
-                </div>
-                <div className="footer-social-icon">
-                  <span>Follow us</span>
-                  <a href="#">
-                    <i className="fab fa-facebook-f facebook-bg"></i>
-                  </a>
-                  <a href="#">
-                    <i className="fab fa-twitter twitter-bg"></i>
-                  </a>
-                  <a href="#">
-                    <i className="fab fa-google-plus-g google-bg"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
+            <div className="flex flex-col gap-1 mt-5">
               <div className="footer-widget">
                 <div className="footer-widget-heading">
-                  <h3>Useful Links</h3>
+                  <h3>Recourses</h3>
                 </div>
                 <ul>
                   <li>
-                    <a href="#">Home</a>
+                    <a href="#">Gift Cards</a>
                   </li>
                   <li>
-                    <a href="#">about</a>
+                    <a href="#">Find a Store</a>
                   </li>
                   <li>
-                    <a href="#">services</a>
+                    <a href="#">Nike Journal</a>
                   </li>
                   <li>
-                    <a href="#">portfolio</a>
+                    <a href="#">Become a Member</a>
                   </li>
                   <li>
-                    <a href="#">Contact</a>
+                    <a href="#">Student Discount</a>
                   </li>
                   <li>
-                    <a href="#">About us</a>
+                    <a href="#">Feedback</a>
                   </li>
                   <li>
-                    <a href="#">Our Services</a>
+                    <a href="#">Promo Codes</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="flex flex-col gap-1 mt-5">
+              <div className="footer-widget">
+                <div className="footer-widget-heading">
+                  <h3>Help</h3>
+                </div>
+                <ul>
+                  <li>
+                    <a href="#">Gift Cards</a>
                   </li>
                   <li>
-                    <a href="#">Expert Team</a>
+                    <a href="#">Find a Store</a>
                   </li>
                   <li>
-                    <a href="#">Contact us</a>
+                    <a href="#">Nike Journal</a>
                   </li>
                   <li>
-                    <a href="#">Latest News</a>
+                    <a href="#">Become a Member</a>
+                  </li>
+                  <li>
+                    <a href="#">Student Discount</a>
+                  </li>
+                  <li>
+                    <a href="#">Feedback</a>
+                  </li>
+                  <li>
+                    <a href="#">Promo Codes</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="flex flex-col gap-1 mt-5">
+              <div className="footer-widget">
+                <div className="footer-widget-heading">
+                  <h3>Company</h3>
+                </div>
+                <ul>
+                  <li>
+                    <a href="#">Gift Cards</a>
+                  </li>
+                  <li>
+                    <a href="#">Find a Store</a>
+                  </li>
+                  <li>
+                    <a href="#">Nike Journal</a>
+                  </li>
+                  <li>
+                    <a href="#">Become a Member</a>
+                  </li>
+                  <li>
+                    <a href="#">Student Discount</a>
+                  </li>
+                  <li>
+                    <a href="#">Feedback</a>
+                  </li>
+                  <li>
+                    <a href="#">Promo Codes</a>
                   </li>
                 </ul>
               </div>
             </div>
             <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
               <div className="footer-widget">
-                <div className="footer-widget-heading">
-                  <h3>Subscribe</h3>
-                </div>
+                <div className="footer-widget-heading"></div>
                 <div className="footer-text mb-25">
                   <p>
                     Don’t miss to subscribe to our new feeds, kindly fill the
@@ -114,7 +123,6 @@ const Footer = () => {
                 </div>
                 <div className="subscribe-form">
                   <form action="#">
-                    <input type="text" placeholder="Email Address" />
                     <button>
                       <i className="fab fa-telegram-plane"></i>
                     </button>
@@ -131,8 +139,8 @@ const Footer = () => {
             <div className="col-xl-6 col-lg-6 text-center text-lg-left">
               <div className="copyright-text">
                 <p>
-                  Copyright &copy; 2018, All Right Reserved{" "}
-                  <a href="https://codepen.io/anupkumar92/">Anup</a>
+                  Copyright &copy; 2024, All Right Reserved{" "}
+                  <a href="https://codepen.io/anupkumar92/">Nike</a>
                 </p>
               </div>
             </div>
