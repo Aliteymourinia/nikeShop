1d0d6a6a9da57a0c99daffb54e56de37a78463d8
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-09 17:37:06 +0330

    updated

--- nikeShop/src/productsData.js
+++ nikeShop/src/productsData.js
@@ -1,6 +1,7 @@
 export const products = {
-  men: {
-    "nike-react-infinity-run-flyknit": {
+  men: [
+    "nike-react-infinity-run-flyknit",
+    {
       id: 1,
       name: "Nike React Infinity Run",
       brand: "NIKE",
@@ -13,9 +14,10 @@ export const products = {
         "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-665455a5-45de-40fb-945f-c1852b82400d/react-infinity-run-flyknit-mens-running-shoe-zX42Nc.jpg",
       slug: "nike-react-infinity-run-flyknit",
       featured: 0,
-      https:""
+      https: "",
     },
-    "nike-react-miler": {
+    "nike-react-miler",
+    {
       id: 2,
       name: "Nike React Miler",
       brand: "NIKE",
@@ -29,7 +31,8 @@ export const products = {
       slug: "nike-react-miler",
       featured: 1,
     },
-    "nike-air-zoom-pegasus-37": {
+    "nike-air-zoom-pegasus-37",
+    {
       id: 3,
       name: "Nike Air Zoom Pegasus",
       brand: "NIKE",
@@ -43,7 +46,8 @@ export const products = {
       slug: "nike-air-zoom-pegasus-37",
       featured: 0,
     },
-    "nike-joyride-run-flyknit": {
+    "nike-joyride-run-flyknit",
+    {
       id: 4,
       name: "Nike Joyride Run Fly",
       brand: "NIKE",
@@ -57,7 +61,8 @@ export const products = {
       slug: "nike-joyride-run-flyknit",
       featured: 0,
     },
-    "nike-mercurial-vapor-13-elite-fg": {
+    "nike-mercurial-vapor-13-elite-fg",
+    {
       id: 5,
       name: "Nike Mercurial Vapor Elite",
       brand: "NIKE",
@@ -71,7 +76,8 @@ export const products = {
       slug: "nike-mercurial-vapor-13-elite-fg",
       featured: 0,
     },
-    "nike-phantom-vision-elite-dynamic": {
+    "nike-phantom-vision-elite-dynamic",
+    {
       id: 6,
       name: "Nike Phantom Vision Elite",
       brand: "NIKE",
@@ -85,7 +91,8 @@ export const products = {
       slug: "nike-phantom-vision-elite-dynamic-fit-fg",
       featured: 0,
     },
-    "nike-phantom-venom-academy-fg": {
+    "nike-phantom-venom-academy-fg",
+    {
       id: 7,
       name: "Nike Phantom Venom",
       brand: "NIKE",
@@ -99,7 +106,8 @@ export const products = {
       slug: "nike-phantom-venom-academy-fg",
       featured: 0,
     },
-    "nike-phantom-venom": {
+    "nike-phantom-venom",
+    {
       id: 8,
       name: "Nike Phantom Academy",
       brand: "NIKE",
@@ -113,7 +121,8 @@ export const products = {
       slug: "nike-phantom-venom-academy-fg",
       featured: 0,
     },
-    "nike-phantom-venom-fg": {
+    "nike-phantom-venom-fg",
+    {
       id: 9,
       name: "Nike Phantom FG",
       brand: "NIKE",
@@ -127,7 +136,8 @@ export const products = {
       slug: "nike-phantom-venom-academy-fg",
       featured: 0,
     },
-    "nike-phantom-venom-academy": {
+    "nike-phantom-venom-academy",
+    {
       id: 10,
       name: "Nike Air Force ",
       brand: "NIKE",
@@ -141,7 +151,8 @@ export const products = {
       slug: "nike-phantom-venom-academy-fg",
       featured: 0,
     },
-    "nike-phantom-venom-academy-f": {
+    "nike-phantom-venom-academy-f",
+    {
       id: 11,
       name: "Nike Air JF",
       brand: "NIKE",
@@ -155,7 +166,8 @@ export const products = {
       slug: "nike-phantom-venom-academy-fg",
       featured: 0,
     },
-    "nike-phantom-vox-elite-fg": {
+    "nike-phantom-vox-elite-fg",
+    {
       id: 12,
       name: "Nike Jelly Run",
       brand: "NIKE",
@@ -169,7 +181,8 @@ export const products = {
       slug: "nike-phantom-venom-academy-fg",
       featured: 0,
     },
-    nike: {
+
+    {
       id: 13,
       name: "Nike Phantom France",
       brand: "NIKE",
@@ -183,7 +196,8 @@ export const products = {
       slug: "nike-phantom-venom-academy-fg",
       featured: 0,
     },
-    nikeee: {
+
+    {
       id: 14,
       name: "Addidas Line Run",
       brand: "NIKE",
@@ -197,7 +211,8 @@ export const products = {
       slug: "nike-phantom-venom-academy-fg",
       featured: 0,
     },
-    nik: {
+
+    {
       id: 15,
       name: "Nike Air Capsule",
       brand: "NIKE",
@@ -211,7 +226,8 @@ export const products = {
       slug: "nike-phantom-venom-academy-fg",
       featured: 0,
     },
-    nikee: {
+
+    {
       id: 16,
       name: "Nike Phantom France Light",
       brand: "NIKE",
@@ -225,10 +241,12 @@ export const products = {
       slug: "nike-phantom-venom-academy-fg",
       featured: 0,
     },
-  },
-  women: {
-    "Nike Air Max ExceeNike Air Max Excee": {
-      id: 1,
+  ],
+
+  women: [
+    "Nike Air Max ExceeNike Air Max Excee",
+    {
+      id: 17,
       name: "Nike Air Max Excee",
       brand: "NIKE",
       gender: "WOMEN",
@@ -239,8 +257,9 @@ export const products = {
       imageURL:
         "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/6bed1e64-09ed-4f2d-90c7-6d5e0f197ff9/air-max-excee-womens-shoes-jKsgMj.png",
     },
-    "Nike Air Max 90 Futura": {
-      id: 2,
+    "Nike Air Max 90 Futura",
+    {
+      id: 18,
       name: "Nike Air Max 90 Futura",
       brand: "NIKE",
       gender: "WOMEN",
@@ -251,8 +270,9 @@ export const products = {
       imageURL:
         "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/f4b7d3bf-12ab-45d1-9748-08c628fa6f8b/air-max-90-futura-womens-shoes-kvRZ4h.png",
     },
-    "Nike Air Vapor Max 2023": {
-      id: 3,
+    "Nike Air Vapor Max 2023",
+    {
+      id: 19,
       name: "Nike Air Vapor Max 2023",
       brand: "NIKE",
       gender: "WOMEN",
@@ -263,8 +283,9 @@ export const products = {
       imageURL:
         "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/833bdbe5-7a00-436e-9e8a-d20006bf24ed/air-vapormax-2023-flyknit-womens-shoes-HtMgGW.png",
     },
-    "Nike Air Max Premium": {
-      id: 4,
+    "Nike Air Max Premium",
+    {
+      id: 20,
       name: "Nike Air Max Premium",
       brand: "NIKE",
       gender: "WOMEN",
@@ -275,7 +296,8 @@ export const products = {
       imageURL:
         "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/bf6f67cc-bc9c-4ff0-993e-ec6c5368dbbe/air-max-1-86-premium-shoes-pqMlR0.png",
     },
-    "Nike Air Max Isla": {
+    "Nike Air Max Isla",
+    {
       id: 5,
       name: "Nike Air Max Isla",
       brand: "NIKE",
@@ -287,7 +309,8 @@ export const products = {
       imageURL:
         "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4a884d25-ce26-4078-8a7d-5f06a59ef165/air-max-isla-womens-sandals-TSzfGq.png",
     },
-    "Nike Air Max Serena": {
+    "Nike Air Max Serena",
+    {
       id: 6,
       name: "Nike Air Max Serena",
       brand: "NIKE",
@@ -299,7 +322,8 @@ export const products = {
       imageURL:
         "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/b3512cdd-d117-45a1-838e-9e3bafa68821/air-max-1-x-serena-williams-design-crew-womens-shoes-dWGlVf.png",
     },
-    "Nike Air Max Ishod": {
+    "Nike Air Max Ishod",
+    {
       id: 7,
       name: "Nike Air Max Ishod",
       brand: "NIKE",
@@ -311,7 +335,8 @@ export const products = {
       imageURL:
         "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/3e2b5818-9b39-4511-9e50-efbc51ca97cc/air-max-ishod-mens-shoes-DwRtpQ.png",
     },
-    "Nike Air Max Dawn": {
+    "Nike Air Max Dawn",
+    {
       id: 8,
       name: "Nike Air Max Dawn",
       brand: "NIKE",
@@ -323,7 +348,8 @@ export const products = {
       imageURL:
         "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/1179fcfd-66fb-47fd-abd6-f2291c7599b9/air-max-dawn-womens-shoes-224mWf.png",
     },
-    "Nike Air Vapormax Plus": {
+    "Nike Air Vapormax Plus",
+    {
       id: 9,
       name: "Nike Air Vapormax Plus",
       brand: "NIKE",
@@ -335,7 +361,8 @@ export const products = {
       imageURL:
         "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/aebf7012-706b-4106-9813-8479e169b65c/air-vapormax-plus-womens-shoes-xbt7zf.png",
     },
-    "Nike Air Max P27": {
+    "Nike Air Max P27",
+    {
       id: 10,
       name: "Nike Air Max P27",
       brand: "NIKE",
@@ -347,7 +374,8 @@ export const products = {
       imageURL:
         "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/aff58eab-0dbe-4330-a443-0c2e4f9ce5a1/air-max-1-87-premium-womens-shoes-FWHTjq.png",
     },
-    "Nike Air Max Axis": {
+    "Nike Air Max Axis",
+    {
       id: 11,
       name: "Nike Air Max Axis",
       brand: "NIKE",
@@ -359,7 +387,8 @@ export const products = {
       imageURL:
         "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/5458a9ae-1108-4e41-b051-903b60ba06d7/air-max-axis-womens-shoes-cCWT9q.png",
     },
-    "Nike Air Max Williams": {
+    "Nike Air Max Williams",
+    {
       id: 12,
       name: "Nike Air Max Williams",
       brand: "NIKE",
@@ -371,9 +400,10 @@ export const products = {
       imageURL:
         "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/9f3a9fe8-2620-4d09-9981-45916772ee82/air-max-1-x-serena-williams-design-crew-shoes-rXLHBc.png",
     },
-  },
-  kids: {
-    "Nike Force Low": {
+  ],
+  kids: [
+    "Nike Force Low",
+    {
       id: 1,
       name: "Nike Force Low",
       brand: "NIKE",
@@ -387,7 +417,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Nike Air Max EasyOn": {
+    "Nike Air Max EasyOn",
+    {
       id: 2,
       name: "Nike Air Max EasyOn",
       brand: "NIKE",
@@ -401,7 +432,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Nike Air Max 270": {
+    "Nike Air Max 270",
+    {
       id: 3,
       name: "Nike Air Max 270",
       brand: "NIKE",
@@ -415,7 +447,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Nike Blazer Mid 77": {
+    "Nike Blazer Mid 77",
+    {
       id: 4,
       name: "Nike Blazer Mid 77",
       brand: "NIKE",
@@ -429,7 +462,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Nike Air Max SE EasyOn": {
+    "Nike Air Max SE EasyOn",
+    {
       id: 5,
       name: "Nike Air Max SE EasyOn",
       brand: "NIKE",
@@ -443,7 +477,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Nike Dunk Low": {
+    "Nike Dunk Low",
+    {
       id: 6,
       name: "Nike Dunk Low",
       brand: "NIKE",
@@ -457,7 +492,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Nike Court Legacy": {
+    "Nike Court Legacy",
+    {
       id: 7,
       name: "Nike Court Legacy",
       brand: "NIKE",
@@ -471,7 +507,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Nike Dunk Lows": {
+    "Nike Dunk Lows",
+    {
       id: 8,
       name: "Nike Dunk Lows",
       brand: "NIKE",
@@ -485,7 +522,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Nike Flex Runner 3": {
+    "Nike Flex Runner 3",
+    {
       id: 9,
       name: "Nike Flex Runner 3",
       brand: "NIKE",
@@ -499,7 +537,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Nike GT Cut SE": {
+    "Nike GT Cut SE",
+    {
       id: 10,
       name: "Nike GT Cut SE",
       brand: "NIKE",
@@ -513,7 +552,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Nike Star Runner 4": {
+    "Nike Star Runner 4",
+    {
       id: 11,
       name: "Nike Star Runner 4",
       brand: "NIKE",
@@ -527,7 +567,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Nike Court Borough Mid": {
+    "Nike Court Borough Mid",
+    {
       id: 12,
       name: "Nike Court Borough Mid",
       brand: "NIKE",
@@ -541,9 +582,10 @@ export const products = {
       slug: "",
       featured: 0,
     },
-  },
-  customize: {
-    "Sabrina 2 Conductor": {
+  ],
+  customize: [
+    "Sabrina 2 Conductor",
+    {
       id: 1,
       name: "Sabrina 2 Conductor",
       brand: "NIKE",
@@ -557,7 +599,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Lebron XXI": {
+    "Lebron XXI",
+    {
       id: 2,
       name: "Lebron XXI",
       brand: "NIKE",
@@ -571,7 +614,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Lebron NXXT Gen": {
+    "Lebron NXXT Gen",
+    {
       id: 3,
       name: "Lebron NXXT Gen",
       brand: "NIKE",
@@ -585,7 +629,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Sabrina 2 Court Vision": {
+    "Sabrina 2 Court Vision",
+    {
       id: 4,
       name: "Sabrina 2 Court Vision",
       brand: "NIKE",
@@ -599,7 +644,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Ja Team Bank": {
+    "Ja Team Bank",
+    {
       id: 5,
       name: "Ja Team Bank",
       brand: "NIKE",
@@ -613,7 +659,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Lebron XXI Queen Counch": {
+    "Lebron XXI Queen Counch",
+    {
       id: 6,
       name: "Lebron XXI Queen Counch",
       brand: "NIKE",
@@ -627,7 +674,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "KD17 Team Bank": {
+    "KD17 Team Bank",
+    {
       id: 7,
       name: "KD17 Team Bank",
       brand: "NIKE",
@@ -641,7 +689,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Nike GT Cut Academy": {
+    "Nike GT Cut Academy",
+    {
       id: 8,
       name: "Nike GT Cut Academy",
       brand: "NIKE",
@@ -655,7 +704,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Nike GT Jump 2": {
+    "Nike GT Jump 2",
+    {
       id: 9,
       name: "Nike GT Jump 2",
       brand: "NIKE",
@@ -669,7 +719,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Nike GT Academy Cut": {
+    "Nike GT Academy Cut",
+    {
       id: 10,
       name: "Nike GT Academy Cut",
       brand: "NIKE",
@@ -683,7 +734,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Lebron Witness 8": {
+    "Lebron Witness 8",
+    {
       id: 11,
       name: "Lebron Witness 8",
       brand: "NIKE",
@@ -697,7 +749,8 @@ export const products = {
       slug: "",
       featured: 0,
     },
-    "Nike Precision 7": {
+    "Nike Precision 7",
+    {
       id: 12,
       name: "Nike Precision 7",
       brand: "NIKE",
@@ -711,7 +764,7 @@ export const products = {
       slug: "",
       featured: 0,
     },
-  },
+  ],
 };
 
 export default products;
