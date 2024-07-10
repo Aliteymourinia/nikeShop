948e683913ff8e541b7c7da92dd9c6c2b36a29cb
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-10 13:30:56 +0330

    updated

--- nikeShop/src/components/ProductDetails.jsx
+++ nikeShop/src/components/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-
+import "../styles/ProductDetails.css";
 const ProductDetails = ({ products }) => {
   const { id } = useParams();
   const allProducts = {
@@ -19,15 +19,15 @@ const ProductDetails = ({ products }) => {
   }
 
   return (
-    <div className="product-details flex p-3 pl-24">
+    <div className="product-details flex p-3 pl-28">
       <div>
         <img
           src={product.imageURL}
           alt={product.name}
-          className="w-4/5 rounded-lg"
+          className="w-4/5 rounded-lg h-[90%] object-cover"
         />
       </div>
-      <div className="Description-Container -mx-6 mt-1">
+      <div className="Description-Container  mt-1">
         <h2 className="text-2xl">{product.name}</h2>
         <p className="mt-1 text-gray-700">{product.gender}'s Shoes</p>
         <p className="text-lg font-semibold mt-1">${product.price}</p>
@@ -35,27 +35,27 @@ const ProductDetails = ({ products }) => {
           <h1 className="-ml-4 ">Select Size</h1>
 
           <tbody className="">
-            <tr className="parent [&>*]:border [&>*]:border-gray-300 [&>*]:p-2 [&>*]:rounded-md">
+            <tr className="parent [&>*]:border [&>*]:border-gray-300 [&>*]:p-2 [&>*]:rounded-md [&>*]:text-gray-800  ">
               <td className="">US 2.5</td>
               <td className="">US 3</td>
               <td className="">US 3.5</td>
             </tr>
-            <tr className="parent [&>*]:border [&>*]:border-gray-300 [&>*]:p-2 [&>*]:rounded-md">
+            <tr className="parent [&>*]:border [&>*]:border-gray-300 [&>*]:p-2 [&>*]:rounded-md [&>*]:text-gray-800">
               <td className="">US 4</td>
               <td className="">US 4.5</td>
               <td className="">US 5</td>
             </tr>
-            <tr className="parent [&>*]:border [&>*]:border-gray-300 [&>*]:p-2 [&>*]:rounded-md">
+            <tr className="parent [&>*]:border [&>*]:border-gray-300 [&>*]:p-2 [&>*]:rounded-md [&>*]:text-gray-800">
               <td className="">US 5.5</td>
               <td className="">US 6</td>
               <td className="">US 6.5</td>
             </tr>
-            <tr className="parent [&>*]:border [&>*]:border-gray-300 [&>*]:p-2 [&>*]:rounded-md">
+            <tr className="parent [&>*]:border [&>*]:border-gray-300 [&>*]:p-2 [&>*]:rounded-md [&>*]:text-gray-800">
               <td className="">US 7</td>
               <td className="">US 7.5</td>
               <td className="">US 8</td>
             </tr>
-            <tr className="parent [&>*]:border [&>*]:border-gray-300 [&>*]:p-2 [&>*]:rounded-md">
+            <tr className="parent [&>*]:border [&>*]:border-gray-300 [&>*]:p-2 [&>*]:rounded-md [&>*]:text-gray-800">
               <td className="">US 8.5</td>
               <td className="">US 9</td>
               <td className="">US 9.5</td>
