8d1e3d10c479d5318815ad250800b359a04d4602
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-10 11:17:23 +0330

    updated

--- nikeShop/src/components/ProductDetails.jsx
+++ nikeShop/src/components/ProductDetails.jsx
@@ -23,10 +23,37 @@ const ProductDetails = ({ products }) => {
       <div>
         <img src={product.imageURL} alt={product.name} className="w-4/5" />
       </div>
-      <div className="-mx-6">
+      <div className="Description-Container -mx-6 mt-1">
         <h2 className="text-2xl">{product.name}</h2>
         <p className="mt-1 text-gray-700">{product.gender}'s Shoes</p>
         <p className="text-lg font-semibold mt-2">${product.price}</p>
+        <table>
+          <tr>
+            <td>US 2.5</td>
+            <td>US 3</td>
+            <td>US 3.5</td>
+          </tr>
+          <tr>
+            <td>US 4</td>
+            <td>US 4.5</td>
+            <td>US 5</td>
+          </tr>
+          <tr>
+            <td>US 5.5</td>
+            <td>US 6</td>
+            <td>US 6.5</td>
+          </tr>
+          <tr>
+            <td>US 7</td>
+            <td>US 7.5</td>
+            <td>US 8</td>
+          </tr>
+          <tr>
+            <td>US 8.5</td>
+            <td>US 9</td>
+            <td>US 9.5</td>
+          </tr>
+        </table>
       </div>
     </div>
   );
