ae5e2d64e0b5af0c682eb3390542181ff43f3fd6
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-10 10:28:15 +0330

    updated

--- nikeShop/src/components/ProductListing.jsx
+++ nikeShop/src/components/ProductListing.jsx
@@ -13,13 +13,13 @@ const ProductListing = ({ products }) => {
         >
           <Link to={`/product/${product.id}`}>
             <img className="w-full" src={product.imageURL} alt={product.name} />
+            <div className="ProductDetail flex flex-col w-full bg-white p-2">
+              <h3 className="font-bold">{product.name}</h3>
+              <h2 className="text-gray-500 font-sans">{product.category}</h2>
+              <h2 className="text-gray-500 font-sans">{product.gender}</h2>
+              <h2 className="font-bold text-lg font-sans">${product.price}</h2>
+            </div>
           </Link>
-          <div className="ProductDetail flex flex-col w-full bg-white p-2">
-            <h3 className="font-bold">{product.name}</h3>
-            <h2 className="text-gray-500 font-sans">{product.category}</h2>
-            <h2 className="text-gray-500 font-sans">{product.gender}</h2>
-            <h2 className="font-bold text-lg font-sans">${product.price}</h2>
-          </div>
         </div>
       ))}
     </div>
