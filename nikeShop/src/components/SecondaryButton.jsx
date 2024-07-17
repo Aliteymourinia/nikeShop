762e4a1f9e5a8cdd6f51159ae1060c510527cdac
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-17 19:21:04 +0330

    updated

--- nikeShop/src/components/SecondaryButton.jsx
+++ nikeShop/src/components/SecondaryButton.jsx
@@ -6,9 +6,9 @@ function SecondaryButton({ text, className, icon }) {
   return (
     <div>
       <button
-        className={`bg-white text-black font-bold hover:bg-black active:scale-90 ${className}`}
+        className={` px-5 bg-white text-black font-bold hover:bg-black hover:text-white active:scale-90 ${className}`}
       >
-        {text} {icon && <span className="absolute">{icon}</span>}
+        {text} {icon && <span className="absolute mx-2">{icon}</span>}
       </button>
     </div>
   );
