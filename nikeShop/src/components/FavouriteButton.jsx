ac1ef61e0ef5fa709b1ee0d66e50aaf551044270
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-18 12:37:13 +0330

    added onClick as props to the component

--- nikeShop/src/components/FavouriteButton.jsx
+++ nikeShop/src/components/FavouriteButton.jsx
@@ -4,6 +4,7 @@ function FavouriteButton({ text, className, icon, onClick }) {
   return (
     <div>
       <button
+        onClick={onClick}
         className={` px-5 bg-white text-black font-bold hover:bg-black  active:scale-90 ${className}`}
       >
         {text} {icon && <span className="absolute mx-2">{icon}</span>}
