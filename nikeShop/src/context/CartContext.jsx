f44a81278450de99226a4106fe878d9284452775
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-18 20:01:22 +0330

    created cart context folder and component

--- nikeShop/src/context/CartContext.jsx
+++ nikeShop/src/context/CartContext.jsx
@@ -1,1 +1,26 @@
 // this component will hold the cart state and provide functions to modify it.
+import React, { createContext, useState, useContext } from "react";
+
+const CartContext = createContext();
+
+export const useCart = () => useContext(CCartContextart);
+
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+  const [notificationCount, setNotificationCount] = useState(0);
+
+  const addToCart = (item) => {
+    setCartItems([...cartItems, item]);
+    setNotificationCount(notificationCount + 1);
+  };
+
+  const clearNotification = () => setNotificationCount(0);
+
+  return (
+    <CartContext.Provider
+      value={{ cartItems, addToCart, notificationCount, clearNotification }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
