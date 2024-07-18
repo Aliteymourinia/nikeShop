daed74f7f4a2ec948d91c9bfed50c5ff7292ecdc
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-18 20:03:39 +0330

    created cart context folder and component

--- nikeShop/src/context/CartContext.jsx
+++ nikeShop/src/context/CartContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState, useContext } from "react";
 
 const CartContext = createContext();
 
-export const useCart = () => useContext(CCartContextart);
+export const useCart = () => useContext(CartContext);
 
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
