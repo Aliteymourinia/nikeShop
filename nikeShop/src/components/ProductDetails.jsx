2e438995b275197ff2a9574ef9388359c68523e2
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-23 10:39:46 +0330

    updated

--- nikeShop/src/components/ProductDetails.jsx
+++ nikeShop/src/components/ProductDetails.jsx
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useCart } from "../context/CartContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "../styles/ProductDetails.css";
 import PrimaryButton from "./PrimaryButton";
 import SecondaryButton from "./SecondaryButton";
 import AirGreen from "../assets/AirGreen.png";
 import FavouriteButton from "./FavouriteButton";
 import Alert from "@mui/material/Alert";
 import SimilarCards from "./SimilarCards";
+import "../styles/ProductDetails.css";
 
 const HeartIcon = (
   <svg
