cafe860fd17dc80f0e2bece1fc52eb84ded81b41
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-23 10:01:53 +0330

    updated

--- nikeShop/src/components/CartIcon.jsx
+++ nikeShop/src/components/CartIcon.jsx
@@ -2,20 +2,27 @@ import React from "react";
 import { useCart } from "../context/CartContext";
 
 const CartIcon = () => {
-  const { notificationCount, clearNotification } = useCart();
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    clearNotification();
-    navigate("/cart");
-  };
+  const { cartItemCount } = useCart();
 
   return (
-    <div className="relative" onClick={handleClick}>
-      <FontAwesomeIcon icon={faShoppingCart} className="text-2xl" />
-      {notificationCount > 0 && (
-        <span className="absolute top-0 right-0 inline-block w-6 h-6 bg-red-500 text-white text-xs font-bold rounded-full text-center">
-          {notificationCount}
+    <div className="relative">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2 8m10-8l2 8m-6-4h4"
+        />
+      </svg>
+      {cartItemCount > 0 && (
+        <span className="absolute -top-2 -right-2 rounded-full bg-red-500 text-white text-xs px-2 py-1">
+          {cartItemCount}
         </span>
       )}
     </div>
