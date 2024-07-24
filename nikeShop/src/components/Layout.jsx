724d14067ebd8ae2689c8bba214bb8eabbfcf1d7
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-07-24 17:12:53 +0330

    updated

--- nikeShop/src/components/Layout.jsx
+++ nikeShop/src/components/Layout.jsx
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 import CartIcon from "./CartIcon";
+import Cart from "./Cart";
 
 const Layout = () => {
   return (
@@ -11,6 +12,7 @@ const Layout = () => {
       <div className="flex-grow">
         <Outlet />
       </div>
+      <Cart />
       <Footer />
     </div>
   );
